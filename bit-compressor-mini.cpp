3bd3d09e541307da64ac1580d1e550ec64e2955b
EdvinAlvarado <ealvarado301@gmail.com>
2019-04-16 23:49:28 -0400

    abondoned for now

--- bit-compressor-mini.cpp
+++ bit-compressor-mini.cpp
@@ -16,7 +16,7 @@ void printvector(vector<T> &v) {
 
 int main () {
 	stringstream ss;
-	string text_sample = "How are you, Jon?";
+	string text_sample = "hello.";
 	int l = text_sample.length();
 	// cout << "text length: " << l << endl;	
 	// filling the string stream with the bit data.
@@ -31,13 +31,13 @@ int main () {
 
 	// different block sizes
 	int num_of_blocks = 1;
-	for (int block_size = hexstring.length() / 2; block_size >= 4; block_size /= 2) {
+	for (int block_size = hexstring.length() / 2; block_size >= 8; block_size /= 2) {
 		num_of_blocks *= 2;
 		cout << "number of blocks: " << num_of_blocks << endl;
 		cout << "block size: " << block_size << endl;
 
 		// prints dynamic even sized blocks of hex string.
-		vector <string> blocks;
+		vector<string> blocks;
 		for (int n = 0; n < num_of_blocks; n++) {
 			// cout << "n = " << n << endl;
 			string block_string = hexstring.substr( (block_size * n),  block_size); // substr(location, length)
@@ -46,21 +46,24 @@ int main () {
 		}
 		// cout << endl;
 		printvector<string>(blocks);
-		cout << "finished vectors" << endl << endl;
+		cout << "blocks" << endl << endl;
+		// stringstream compressor_ss;
+		// compressor_ss << blocks[0];
 		for (int i = 0; i < blocks.size()-1; i++) {
-
-			cout << "block i\n" << blocks[i] << endl;
-			stringstream compressor_ss;
-			compressor_ss << blocks[i];
-			for (int j = 1; j < blocks.size(); j++) {				
+			cout << "block i: " << i << "\n" << blocks[i] << endl;			
+			for (int j = 1+i; j < blocks.size(); j++) {
+				cout << "block j: " << j << "\n" << blocks[j] << endl;		
 				if (blocks[i] == blocks[j]) {
-					compressor_ss << "p" << j-i << block_size;
-				} else {
-					compressor_ss << blocks[j];
+					// compressor_ss << "p" << j-i << block_size;
+					string coder = "P" + to_string(j-i) + "L" + to_string(block_size);
+					blocks[j] = coder;
 				}
 			}
-			cout << "compressed binary:\n" << compressor_ss.str() << endl << endl;
-			compressor_ss.str("");
+			// cout << "compressed binary:\n" << compressor_ss.str() << endl << endl;
+			// compressor_ss.str("");
+			cout << endl << "resulting vector" << endl;
+			printvector<string>(blocks);
+			cout << endl;
 		}
 	}
 }
